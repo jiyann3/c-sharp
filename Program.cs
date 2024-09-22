dc6d4203b2cd9893f2520a46066c934a24f00321
Jiyan Özmaya <ozj81018@gmail.com>
2024-09-23 01:48:29 +0300

    c# karşılaştırma
    c# basit kod örneği

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Sayi gir:");
+int sayi = Convert.ToInt32(Console.ReadLine());
+
+if(sayi<=0){
+    if (sayi <0){
+        Console.WriteLine("Pozitif bir sayi gir");
+    }
+ else if(sayi==0){
+    Console.WriteLine("0 Sayısı girilemez");
+ }
+}
+
+else if(sayi %10== 0){
+    Console.WriteLine("Girdiğiniz sayı 10'un katıdır");
+}
+
+else{
+    Console.WriteLine("Girdiğiniz sayı 10'un katı değildir");
+}
